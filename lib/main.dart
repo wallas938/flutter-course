de830bb0f26d3661633774b830c883538da2eb36
wallas938 <drame.sissako001@gmail.com>
2024-06-07 17:27:31 +0200

    todo app part1 with todo removal

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,247 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 const userName = 'USER';
 
 void main() {
-  runApp(const MaterialApp(home: DynamicBackground()));
+  runApp(const MyApp());
+  // runApp(const MaterialApp(home: Counter()));
+  // runApp(const MaterialApp(home: DynamicBackground()));
   // runApp(const MaterialApp(home: RowsAndColumns()));
   // runApp(const MaterialApp(home: SimpleStatelessWidget()));
   // runApp(const MaterialApp( home: Scaffold( body: Center( child: UserButton()))));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return const MaterialApp(home: MyTextField());
+    return const MaterialApp(home: MyListBuilderView());
+  }
+}
+
+class MyListBuilderView extends StatelessWidget {
+  
+  const MyListBuilderView({super.key});
+
+  static const items = ["item1", "item2", "item3"];
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      title: "Example",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("App Bar Title"),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            return ListTile(
+              title: Text(item),
+              onTap: () {
+                print("You just tapped on $item");
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+}
+
+class MyListView extends StatefulWidget {
+  const MyListView({super.key});
+
+  @override
+  State<MyListView> createState() => _MyListView();
+}
+
+class _MyListView extends State<MyListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My List View"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const  Text("Person One"),
+            // subtitle: const  Text("email@one.fr"),
+            // trailing: const  Icon(Icons.arrow_forward),
+            onTap: () => print("ListTile 1 was Tapped!"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const  Text("Person Two"),
+            // subtitle: const  Text("email@one.fr"),
+            // trailing: const  Icon(Icons.arrow_forward),
+            onTap: () => print("ListTile 2 was Tapped!"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const  Text("Person Three"),
+            // subtitle: const  Text("email@one.fr"),
+            // trailing: const  Icon(Icons.arrow_forward),
+            onTap: () => print("ListTile 3 was Tapped!"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const  Text("Person Four"),
+            // subtitle: const  Text("email@one.fr"),
+            // trailing: const  Icon(Icons.arrow_forward),
+            onTap: () => print("ListTile 4 was Tapped!"),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const  Text("Person Five"),
+            // subtitle: const  Text("email@one.fr"),
+            // trailing: const  Icon(Icons.arrow_forward),
+            onTap: () => print("ListTile 5 was Tapped!"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyDialogBox extends StatefulWidget {
+  const MyDialogBox({super.key});
+
+  @override
+  State<MyDialogBox> createState() => _MyDialogBox();
+}
+
+class _MyDialogBox extends State<MyDialogBox> {
+  String _inputText = "";
+
+  void _showInputDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          TextEditingController controller = TextEditingController();
+
+          return AlertDialog(
+            title: const Text("Enter some text!"),
+            content: TextField(
+              controller: controller,
+              decoration: const InputDecoration(hintText: 'Enter Some text'),
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Cancel")),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _inputText = controller.text;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Save"))
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("My new App!"),
+        ),
+        body: Center(
+            child: Text(_inputText.isEmpty
+                ? "Tap the icon to enter text"
+                : "You entered $_inputText")),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _showInputDialog,
+          backgroundColor: Colors.black,
+          child: const Icon(
+            Icons.edit,
+            color: Colors.red,
+          ),
+        ));
+  }
+}
+
+class MyTextField extends StatefulWidget {
+  const MyTextField({super.key});
+
+  @override
+  State<MyTextField> createState() => _MyTextField();
+}
+
+class _MyTextField extends State<MyTextField> {
+  String _inputText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App bar $_inputText !"),
+      ),
+      body: Center(
+        child: TextField(
+          decoration: const InputDecoration(
+            hintText: "Enter some text here!",
+          ),
+          onChanged: (value) {
+            setState(() {
+              _inputText = value;
+            });
+          },
+        ),
+      ),
+      bottomSheet: Container(
+        alignment: Alignment.center,
+        height: 50,
+        child: Text("You typed: $_inputText"),
+      ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  int _count = 0;
+
+  void _increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        body: Center(
+            child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(onPressed: _increment, child: const Text("Push Me!")),
+        Text("You pressed the button $_count times")
+      ],
+    )));
+  }
 }
 
 class DynamicBackground extends StatefulWidget {
@@ -24,12 +258,13 @@ class _DynamicBackgroundState extends State<DynamicBackground> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: GestureDetector(onTap: () {
-        setState(() {
-          isButtonPressed = !isButtonPressed;
-          print(isButtonPressed);
-        });
-      },
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            isButtonPressed = !isButtonPressed;
+            print(isButtonPressed);
+          });
+        },
         child: Container(color: getColor()),
       ),
     );
